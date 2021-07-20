e6d5b978cb53eef29e3bf899125f35166662febb
mateusmosquera <mateusmosquera@hotmail.com>
2021-07-20 11:05:01 -0300

    setup do projeto

---
+++ src/test/java/com/br/zupacademy/mateus/DesafioPixTest.java
@@ -0,0 +1,21 @@
+package com.br.zupacademy.mateus;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class DesafioPixTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
